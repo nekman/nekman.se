b28b208a011e5d85617d11d93d2c7969c268610e
Nils Ekman <nisse.ekman@gmail.com>
2013-01-27 18:37:46 +0100

    parse links with twitter-entities

--- js/twitter/twitterView.js
+++ js/twitter/twitterView.js
@@ -1,13 +1,14 @@
-define('twitterView', ['jquery', 'underscore', 'backbone'], function($, _, Backbone) {
+define('twitterView', ['jquery', 'underscore', 'backbone', 'twitterEntities'], function($, _, Backbone, twitterEntities) {
   'use strict';
   
   return Backbone.View.extend({
     el: '#tweets',
 
     render: function() {
+      
       this.$el.append(
         $('<div>').addClass('tweet')
-                  .text(this.model.text));
+                  .html(twitterEntities.linkify(this.model)));
 
       return this;
     }
