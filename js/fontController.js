081c75d9d0fec3539f1d2a8584e3bf6cc0b63b86
Nils Ekman <nisse.ekman@gmail.com>
2013-01-27 13:45:04 +0100

    Added font controller

---
+++ js/fontController.js
@@ -0,0 +1,20 @@
+define('fontController', ['webfont', 'jquery'], function(webfont, $) {
+	var webFontDeferred = new $.Deferred();
+
+	webfont.load({
+		google: {
+			families: [
+				'Cantarell',
+				'Droid+Serif',
+				'Lato:100:latin'
+			]
+		},
+		active: function() {
+			webFontDeferred.resolve();
+		}
+	});
+
+	return {
+		promise: webFontDeferred
+	};
+});
