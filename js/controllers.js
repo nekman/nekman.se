c21248e4f26a2dff8df00e1171c5e3d999e0a0fd
Nils Ekman <nisse.ekman@gmail.com>
2013-01-27 13:48:20 +0100

    Added fontController

--- js/controllers.js
+++ js/controllers.js
@@ -1,17 +1,35 @@
 /*
- * All controllers
+ * Collection of all controllers (except the main controller).
+ * 
+ * Makes it easier to run same methods on all controllers.
  */
 define('controllers', function() {
+  'use strict';
+
   var controllers = [
+    require('fontController'),
     require('twitterController'),
     require('boxController')
   ];
-
+  
   return {
-    promises: controllers.map(function(controller) {
-        return controller.promise;
+    /*
+     * The collection of all promises
+     */
+    promises: _.map(controllers, function(controller) {
+      return controller.promise;
     }),
 
-    all: controllers
+    /*
+     * The collection of all controllers
+     */
+    all: controllers,
+
+    /*
+     * The collection of view controllers
+     */
+    viewControllers: _.filter(controllers, function(controller) {
+      return typeof controller.render === 'function';
+    })
   };
 });
