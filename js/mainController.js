2cccbc1d0d7fccc1906d41a2e1e0ec2ca6e5fca0
Nils Ekman <nisse.ekman@gmail.com>
2013-01-29 00:50:03 +0100

    Added windowResizeHandler

--- js/mainController.js
+++ js/mainController.js
@@ -1,38 +1,24 @@
 /*
  * The main controller
  */
-define('mainController', ['webfont', 'controllers', 'jquery'], function(webfont, controllers, $) {
+define('mainController', ['webfont', 'controllers', 'jquery', 'windowResizeHandler'], function(webfont, controllers, $, resizeHandler) {
   // Private variables
-  var DEFAULT_WIDTH = 960,
-    $window = $(window),
-    $header = $('h1'),
-    $loading = $('<div>')
-                  .append($('<i>').addClass('icon-spinner icon-spin'))
+  var isIE = $.support.cssFloat,
+      $header = $('h1'),
+      $loading = $('<div>')
+                  .append($('<i>').addClass('icon-spinner icon-spin icon-4x'))
                   .append(' Loading content... '),
 
   // Constructor
   MainController = function() {
-    this.$el = $('#main')
-                .addClass('defaultwidth')
-                .append($loading);
-
-    // Resize header and #main. Store them in memory.
-    this.$resizeNodes = $('header').addClass('defaultwidth').add(this.$el);
-  },
-
-  // Handle resize
-  onresize = function() {
-    var width = $window.width();
+    this.$el = $('#main').append($loading);
     
-    if (width < DEFAULT_WIDTH) {
-      this.$resizeNodes.removeClass('defaultwidth').addClass('minwidth');
-      return;
-    }
+    // Only show gitub-ribbon for none IE-browsers, since they can't handle the element CSS-rotation.
+    $('div.ribbon').css({ display : !isIE });
 
-    if (!this.$resizeNodes.hasClass('defaultwidth')) {
-      this.$resizeNodes.addClass('defaultwidth').removeClass('minwidth');
-    }
-    
+    // Watch header and #main element if window resizes.
+    var $resizeNodes = $('header').add(this.$el);
+    resizeHandler.watch($resizeNodes);
   };
 
   // Public methods
@@ -40,12 +26,10 @@ define('mainController', ['webfont', 'controllers', 'jquery'], function(webfont,
     constructor: MainController,
 
     render: function() {
-      window.onresize = $.proxy(onresize, this);
-      $window.trigger('resize');
-
       var $el = this.$el,
 
-      internalRender = function() {
+      renderViewControllers = function() {
+        // Render all view controllers
         _.each(controllers.viewControllers, function(controller) {
           $el.append(controller.render().$el);
         });
@@ -53,9 +37,9 @@ define('mainController', ['webfont', 'controllers', 'jquery'], function(webfont,
         $loading.hide();
         $header.show();
       };
-
-      // When all promises is resloved, render all views.
-      $.when(controllers.promises).done(internalRender);
+      
+      // When all controllers is loaded, render the view controllers.
+      $.when(controllers.promises).done(renderViewControllers);
     }
   };
 
---
+++ js/windowResizeHandler.js
@@ -0,0 +1,31 @@
+define('windowResizeHandler', ['jquery'], function($) {
+	var DEFAULT_WIDTH = 960,
+		$window = $(window),
+		$resizeNodes,
+	
+	// Handle resize
+	onresize = function() {
+		var width = $window.width();
+
+		if (width < DEFAULT_WIDTH) {
+			$resizeNodes.removeClass('defaultwidth').addClass('minwidth');
+			return;
+		}
+		if (!$resizeNodes.hasClass('defaultwidth')) {
+			$resizeNodes.addClass('defaultwidth').removeClass('minwidth');
+		}
+	};
+
+	return {
+		watch: function($nodes) {
+			if (!$nodes instanceof $) {
+				$nodes = $();
+			}
+
+			$resizeNodes = $nodes.addClass('defaultwidth');
+
+			// Add event handler to window resize, and trigger the event.
+			$window.on('resize', onresize).trigger('resize');
+		}
+	};
+});
