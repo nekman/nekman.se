5ae55205a9094dc72bcf40a3a47077fcbb07c60d
Nils Ekman <nisse.ekman@gmail.com>
2013-02-02 15:47:31 +0100

    Added test for mainView

---
+++ test/views/mainView.spec.js
@@ -0,0 +1,21 @@
+require('../testUtils').setup();
+
+describe('main view', function() {
+	var mainView = require('../../js/main/mainView');
+
+	describe('loading', function() {
+		var $loading = mainView.$el.find('#loading');
+
+		it('should show the loading before rendering', function() {
+			expect($loading.css('display')).toBe('');
+			expect($loading.text()).toMatch(/Loading content/);
+		});
+
+		it('should hide the loading after rendering', function() {
+			mainView.render();
+
+			expect($loading.css('display')).toBe('none');
+		});
+	});
+});
+
