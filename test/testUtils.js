fd5b38c1bb60b04987f83aa139236dd744a8a40e
Nils Ekman <nisse.ekman@gmail.com>
2013-02-02 15:46:42 +0100

    Use backbone and underscore

--- test/testUtils.js
+++ test/testUtils.js
@@ -3,8 +3,17 @@
  */
 exports.setup = function() {
 	global.define = require('define');
-	//fake browser window
-	global.window = require('jsdom').jsdom().createWindow();
+	// Setup a fake browser window
+	global.document = require('jsdom').jsdom('<html><body><div id="main"></div></body></html>');
+	global.navigator = document.navigator;
+	global.window = document.createWindow();
+
+	// Setup jQuery, Underscore and Backbone
 	global.jQuery = global.$ = require('jquery');
+	global._ = require('underscore');
+	global.Backbone = require('backbone');
+
+	// Set Backbone DOM-library
+	Backbone.$ = global.jQuery;
 };
 
